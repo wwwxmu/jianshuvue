b1e87c61a429e67bfeae8b5ded7ad0c97deacbb4
www@stu.xmu.edu.cn <www@stu.xmu.edu.cn>
2018-07-20 15:51:23 +0800

    :tada: first commit

---
+++ front/src/components/demo.vue
@@ -0,0 +1,327 @@
+<template>
+  <div class="main_content">
+    <avatar :image="image_url" size="128"></avatar>
+    <h2><a :href="j_url">{{name}}</a></h2>
+    <h3>新增字数: {{new_word}} &nbsp;&nbsp; 新增喜欢: {{new_like}} &nbsp;&nbsp; 新增文章: {{new_article}} &nbsp;&nbsp; 新增粉丝: {{new_follows}}</h3>
+    <div id="baseInfo"></div>
+    <v-table id="aTable"
+            :columns="columns"
+            :table-data="article_table"
+            :show-vertical-border="false"
+            :multiple-sort="multipleSort"
+            @sort-change="sortChange"
+            row-hover-color="#eee"
+            row-click-color="#edf7ff"
+    ></v-table>
+  </div>
+</template>
+<script>
+import echarts from 'echarts'
+import axios from 'axios'
+import Avatar from 'vue-avatar-component'
+// import bTable from 'bootstrap-vue/es/components/table/table'
+import 'vue-easytable/libs/themes-base/index.css'
+// 导入 table 和 分页组件
+import {VTable, VPagination} from 'vue-easytable'
+export default {
+  components: {Avatar, VTable, VPagination},
+  data () {
+    return {
+      multipleSort: false,
+      pageSize: 20,
+      chart: null,
+      name: null,
+      article_table: [],
+      image_url: null,
+      new_word: 0,
+      new_like: 0,
+      new_article: 0,
+      new_follows: 0,
+      j_url: 'https://www.jianshu.com/u/' + this.$route.params.id,
+      columns: [
+        {
+          field: 'title',
+          title: '标题',
+          width: 400,
+          titleAlign: 'center',
+          columnAlign: 'center',
+          isResize: true
+        },
+        {
+          field: 'link',
+          title: '链接',
+          width: 350,
+          titleAlign: 'center',
+          columnAlign: 'center',
+          isResize: true
+        },
+        {
+          field: 'comment_num',
+          title: '评论数',
+          width: 100,
+          titleAlign: 'center',
+          columnAlign: 'center',
+          orderBy: 'asc',
+          isResize: true
+        },
+        {
+          field: 'heart_num',
+          title: '点赞数',
+          width: 100,
+          titleAlign: 'center',
+          columnAlign: 'center',
+          orderBy: 'asc',
+          isResize: true
+        },
+        {
+          field: 'read_num',
+          title: '阅读量',
+          width: 100,
+          titleAlign: 'center',
+          columnAlign: 'center',
+          orderBy: 'asc',
+          isResize: true
+        }
+      ]
+    }
+  },
+  methods: {
+    baseInfo (id) {
+      this.baseInfoChart = echarts.init(document.getElementById(id))
+      var option = {
+        title: {
+          text: '简书作者基本数据概览',
+          left: 'center',
+          top: 5,
+          textStyle: {
+            fontSize: 16,
+            fontFamily: 'Helvetica',
+            fontWeight: 200
+          }
+        },
+        tooltip: {
+          trigger: 'axis'
+        },
+        legend: {
+          data: ['关注', '粉丝', '文章', '收获喜欢', '字数'],
+          left: 'center',
+          top: 30
+        },
+        grid: {
+          left: '15%',
+          right: '15%'
+        //  bottom: '15%',
+        // containLabel: true
+        },
+        toolbox: {
+          feature: {
+            dataZoom: {
+              yAxisIndex: 'none'
+            },
+            restore: {},
+            saveAsImage: {}
+          }
+        },
+        xAxis: {
+          type: 'category',
+          // boundaryGap: false,
+          data: []
+        },
+        yAxis: [{
+          type: 'value'
+        }, {
+          type: 'value'
+          // splitLine : {show : false}
+        }],
+        dataZoom: [{
+          start: 0,
+          end: 100
+        }, {
+          type: 'inside'
+        }],
+        series: [{
+          name: '关注',
+          type: 'line',
+          itemStyle: {
+            borderWidth: 5,
+            shadowColor: 'rgba(0, 0, 0, 0.5)',
+            shadowBlur: 10
+          },
+          lineStyle: {
+            width: 5,
+            shadowColor: 'rgba(0, 0, 0, 0.5)',
+            shadowBlur: 10
+          },
+          stack: '总量',
+          data: []
+        },
+        {
+          name: '粉丝',
+          type: 'line',
+          itemStyle: {
+            borderWidth: 5,
+            shadowColor: 'rgba(0, 0, 0, 0.5)',
+            shadowBlur: 10
+          },
+          lineStyle: {
+            width: 5,
+            shadowColor: 'rgba(0, 0, 0, 0.5)',
+            shadowBlur: 10
+          },
+          stack: '总量',
+          data: []
+        },
+        {
+          name: '文章',
+          type: 'line',
+          itemStyle: {
+            borderWidth: 5,
+            shadowColor: 'rgba(0, 0, 0, 0.5)',
+            shadowBlur: 10
+          },
+          lineStyle: {
+            width: 5,
+            shadowColor: 'rgba(0, 0, 0, 0.5)',
+            shadowBlur: 10
+          },
+          stack: '总量',
+          data: []
+        },
+        {
+          name: '收获喜欢',
+          type: 'line',
+          itemStyle: {
+            borderWidth: 5,
+            shadowColor: 'rgba(0, 0, 0, 0.5)',
+            shadowBlur: 10
+          },
+          lineStyle: {
+            width: 5,
+            shadowColor: 'rgba(0, 0, 0, 0.5)',
+            shadowBlur: 10
+          },
+          stack: '总量',
+          data: []
+        },
+        {
+          name: '字数',
+          type: 'bar',
+          itemStyle: {
+            color: '#0984e3',
+            barBorderRadius: [10, 10, 0, 0],
+            shadowColor: 'rgba(0, 0, 0, 0.5)',
+            shadowBlur: 10
+          },
+          yAxisIndex: 1,
+          stack: '总量',
+          data: []
+        }]
+      }
+      this.baseInfoChart.setOption(option)
+      const path = `http://localhost:5000/api/base/` + this.$route.params.id
+      axios.get(path).then(response => {
+        this.image_url = response.data.img
+        this.name = response.data.name
+        this.new_word = response.data.new_word
+        this.new_like = response.data.new_like
+        this.new_article = response.data.new_article
+        this.new_follows = response.data.new_follows
+        this.baseInfoChart.setOption({
+          xAxis: {
+            data: response.data.date
+          },
+          series: [
+            {
+              name: '关注',
+              data: response.data.following
+            },
+            {
+              name: '粉丝',
+              data: response.data.follows
+            },
+            {
+              name: '文章',
+              data: response.data.article_nums
+            },
+            {
+              name: '字数',
+              data: response.data.word_nums
+            },
+            {
+              name: '收获喜欢',
+              data: response.data.like_nums
+            }]
+        })
+      }).catch(error => {
+        console.log(error)
+      })
+      window.addEventListener('resize', function () {
+        this.vipSetOption.baseInfoChart.resize()
+      })
+    },
+    sortChange (params) {
+      if (params.comment_num) {
+        this.article_table.sort(function (a, b) {
+          if (params.comment_num === 'asc') {
+            return a.comment_num - b.comment_num
+          } else if (params.comment_num === 'desc') {
+            return b.comment_num - a.comment_num
+          } else {
+            return 0
+          }
+        })
+      }
+      if (params.heart_num) {
+        this.article_table.sort(function (a, b) {
+          if (params.heart_num === 'asc') {
+            return a.heart_num - b.heart_num
+          } else if (params.heart_num === 'desc') {
+            return b.heart_num - a.heart_num
+          } else {
+            return 0
+          }
+        })
+      }
+      if (params.read_num) {
+        this.article_table.sort(function (a, b) {
+          if (params.read_num === 'asc') {
+            return a.read_num - b.read_num
+          } else if (params.read_num === 'desc') {
+            return b.read_num - a.read_num
+          } else {
+            return 0
+          }
+        })
+      }
+    }
+  },
+  mounted () {
+    this.$nextTick(function () {
+      this.baseInfo('baseInfo')
+    })
+  },
+  created: function () {
+    axios.get('http://localhost:5000/api/article/' + this.$route.params.id).then(response => {
+      this.article_table = response.data.article
+    }).catch(error => {
+      console.log(error)
+    })
+  }
+}
+</script>
+<style scoped>
+#baseInfo {
+  position: relative;
+  margin: auto auto;
+  width: 100%;
+  height: 300px;
+}
+a, a:hover {
+    text-decoration: none;
+    color: #74b9ff;
+}
+#aTable {
+  position: relative;
+  margin: auto auto;
+}
+</style>
---
+++ run.py
@@ -0,0 +1,173 @@
+from random import *
+from flask_cors import CORS
+from flask_mongoengine import MongoEngine
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__,
+            static_folder = "./dist/static",
+            template_folder = "./dist")
+cors = CORS(app, resources={"/api/*": {"origins": "*"}})
+
+app.config['MONGODB_SETTINGS'] = {
+    'db': 'jianshu',
+    'host': '127.0.0.1',
+    'port': 27017
+    }
+db = MongoEngine(app)
+
+class Dailyinfo(db.Document):
+    meta = {
+        'collection': 'author_info',
+        'ordering': ['date'],
+        'strict': False,
+    }
+
+    uid = db.StringField()
+    name = db.StringField()
+    img = db.StringField()
+    date = db.StringField()
+    following = db.StringField()
+    follows = db.StringField()
+    article_nums = db.StringField()
+    word_nums = db.StringField()
+    like_nums = db.StringField()
+    article_details = db.ListField()
+    follows_details = db.ListField()
+
+@app.route('/api/base/<uid>')
+def base(uid):
+    baseinfo = Dailyinfo.objects(uid=uid).order_by('-date').all()
+    new_follows = int(baseinfo[1]['follows']) - int(baseinfo[2]['follows'])
+    new_article = int(baseinfo[1]['article_nums']) - int(baseinfo[2]['article_nums'])
+    new_word = int(baseinfo[1]['word_nums']) - int(baseinfo[2]['word_nums'])
+    new_like = int(baseinfo[1]['like_nums']) - int(baseinfo[2]['like_nums'])
+    info = Dailyinfo.objects(uid=uid).all()
+    date = []
+    follows = []
+    following = []
+    article_nums = []
+    word_nums = []
+    like_nums = []
+    read_nums = []
+    review_nums = []
+    most_read = {}
+    most_review = {}
+    most_like = {}
+    follows_words = {}
+    follows_follows = {}
+    follows_likes = {}
+    follows_articles = {}
+    follows_following = {}
+    name = info[1]['name']
+    img = info[1]['img']
+    for i in info:
+        date.append(i['date'])
+        follows.append(i['follows'])
+        following.append(i['following'])
+        article_nums.append(i['article_nums'])
+        word_nums.append(i['word_nums'])
+        like_nums.append(i['like_nums'])
+        read_nums.append(sum([int(a['read_num']) for a in i['article_details']]))
+        review_nums.append(sum([int(a['comment_num']) for a in i['article_details']]))
+        most_read[i['date']] = sorted(i['article_details'], key = lambda e:int(e.__getitem__('read_num')), reverse = True)
+        most_review[i['date']] = sorted(i['article_details'], key = lambda e:int(e.__getitem__('comment_num')), reverse = True)
+        most_like[i['date']] = sorted(i['article_details'], key = lambda e:int(e.__getitem__('heart_num')), reverse = True)
+        follows_words[i['date']] = sum([int(a['word_nums']) for a in i['follows_details']])
+        follows_follows[i['date']] = sum([int(a['follows']) for a in i['follows_details']])
+        follows_likes[i['date']] = sum([int(a['like_nums']) for a in i['follows_details']])
+        follows_articles[i['date']] = sum([int(a['article_nums']) for a in i['follows_details']])
+        follows_following[i['date']] = sum([int(a['following']) for a in i['follows_details']])
+    res = {
+        'follows':follows,
+        'following': following,
+        'article_nums': article_nums,
+        'word_nums': word_nums,
+        'like_nums': like_nums,
+        'read_nums': read_nums,
+        'review_nums': review_nums,
+        'most_read': most_read,
+        'most_review': most_review,
+        'most_like': most_like,
+        'follows_words': follows_words,
+        'follows_follows': follows_follows,
+        'follows_likes': follows_likes,
+        'follows_articles': follows_articles,
+        'follows_following': follows_following,
+        'date': date,
+        'name': name,
+        'img': img,
+        'uid': uid,
+        'new_word': new_word,
+        'new_like': new_like,
+        'new_article': new_article,
+        'new_follows': new_follows
+    }
+    return jsonify(res)
+
+@app.route('/api/article/<uid>')
+def article(uid):
+    a = Dailyinfo.objects(uid=uid).order_by('-date').all()
+    #article = sorted(a[1]['article_details'], key = lambda e:int(e.__getitem__('heart_num')), reverse = True) if a else None
+    article = []
+    for i in a[1]['article_details']:
+        d = i.copy()
+        s = "https://www.jianshu.com"+i['link']
+        d.update({'link': s})
+        article.append(d)
+    res = {
+        'article': article
+    }
+    return jsonify(res)
+
+
+@app.route('/api/follows/<uid>')
+def follows(uid):
+    info = Dailyinfo.objects(uid=uid).all()
+    date = []
+    follows_words = {}
+    follows_follows = {}
+    follows_likes = {}
+    follows_articles = {}
+    follows_following = {}
+    follows_most_words = {}
+    follows_most_follows = {}
+    follows_most_likes = {}
+    follows_most_articles = {}
+    follows_most_following = {}
+    for i in info:
+        date.append(i['date'])
+        follows_follows[i['date']] = sum([int(a['follows']) for a in i['follows_details']])
+        follows_likes[i['date']] = sum([int(a['like_nums']) for a in i['follows_details']])
+        follows_articles[i['date']] = sum([int(a['article_nums']) for a in i['follows_details']])
+        follows_following[i['date']] = sum([int(a['following']) for a in i['follows_details']])
+        follows_most_words[i['date']] = sorted(i['follows_details'], key = lambda e:int(e.__getitem__('word_nums')), reverse = True)
+        follows_most_follows[i['date']] = sorted(i['follows_details'], key = lambda e:int(e.__getitem__('follows')), reverse = True)
+        follows_most_likes[i['date']] = sorted(i['follows_details'], key = lambda e:int(e.__getitem__('like_nums')), reverse = True)
+        follows_most_articles[i['date']] = sorted(i['follows_details'], key = lambda e:int(e.__getitem__('article_nums')), reverse = True)
+        follows_most_following[i['date']] = sorted(i['follows_details'], key = lambda e:int(e.__getitem__('following')), reverse = True)
+    res = {
+        'follows_words': follows_words,
+        'follows_follows': follows_follows,
+        'follows_likes': follows_likes,
+        'follows_articles': follows_articles,
+        'follows_following': follows_following,
+        'follows_most_words': follows_most_words,
+        'follows_most_follows': follows_most_follows,
+        'follows_most_likes': follows_most_likes,
+        'follows_most_articles': follows_most_articles,
+        'follows_most_following': follows_most_following,
+        'date': date
+    }
+    return jsonify(res)
+
+@app.route('/api/random')
+def random_number():
+    response = {
+        'randomNumber': randint(1, 100)
+    }
+    return jsonify(response)
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return render_template("index.html")
