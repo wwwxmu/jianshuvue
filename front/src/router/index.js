cd555380c860e4d5463f32c338503668a157d160
www@stu.xmu.edu.cn <www@stu.xmu.edu.cn>
2018-08-06 14:24:40 +0800

    修改 HTTPS 协议

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -3,8 +3,8 @@ import Router from 'vue-router'
 const routerOptions = [
 //  {path: '/', component: 'Home'},
 //  {path: '/about', component: 'About'},
-  {path: '/j/:id', component: 'demo'},
-  {path: '*', component: 'NotFound'}
+  {path: '/j/:id', component: 'demo', meta: {title: '简书'}},
+  {path: '*', component: 'NotFound', meta: {title: '找不到页面'}}
 ]
 const routes = routerOptions.map(route => {
   return {
